2cf7b8347a3d8d3cf19efae160a83b1c94435ec3
Gabriel <gribeiro237@gmail.com>
2021-09-02 18:48:41 -0300

    Requisito 2

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import EmailInput from '../../Components/EmailInput';
 import LoginInput from '../../Components/LoginInput';
 import logo from '../../trivia.png';
@@ -26,6 +27,20 @@ class Login extends React.Component {
     return !(login.length !== 0 && email.length !== 0);
   }
 
+  async handleClick() {
+    const { history } = this.props;
+    const token = await this.fetchToken();
+    console.log(token);
+    history.push('/trivia');
+  }
+
+  async fetchToken() {
+    const urlToken = await fetch('https://opentdb.com/api_token.php?command=request');
+    const { token } = await urlToken.json();
+    localStorage.setItem('token', token);
+    return token;
+  }
+
   render() {
     const { login, email } = this.state;
     console.log(this.checkFields());
@@ -48,6 +63,7 @@ class Login extends React.Component {
             type="button"
             data-testid="btn-play"
             disabled={ this.checkFields() }
+            onClick={ () => this.handleClick() }
           >
             Jogar
 
@@ -59,5 +75,11 @@ class Login extends React.Component {
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;

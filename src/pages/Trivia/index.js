c493cc0d100f97c020d906331b82219e5f088c2d
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-09-03 16:40:00 -0300

    resolve ex 4

--- src/pages/Trivia/index.js
+++ src/pages/Trivia/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from '../../Components/Header';
 
 class Trivia extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Teste</h1>
-      </div>
+      <main>
+        <Header score="0" playerName="joao" email="jvictorfranca@yahoo.com.br" />
+      </main>
     );
   }
 }

4cfdcfd53b7cb1e87a8cb484a316259736efec0a
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-09-13 22:33:47 -0300

    Updates

---
+++ precache-manifest.b4ec19cd358f15695e08eb1a9a9b3a5f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572ee02ed12a0d33616a2ea9f9b59b1a",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "d76e41bcb06e94a67123",
+    "url": "/trivia/static/css/main.760c8deb.chunk.css"
+  },
+  {
+    "revision": "776cfd3bd9d5aa322216",
+    "url": "/trivia/static/js/2.7218f2fe.chunk.js"
+  },
+  {
+    "revision": "a38179c3a123c4a544eacdbb77f01e24",
+    "url": "/trivia/static/js/2.7218f2fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d76e41bcb06e94a67123",
+    "url": "/trivia/static/js/main.1c90f807.chunk.js"
+  },
+  {
+    "revision": "23dbb679c736924e4b1a",
+    "url": "/trivia/static/js/runtime-main.6e763e45.js"
+  },
+  {
+    "revision": "8d3ce9850c6236a36ddb07b2a576275d",
+    "url": "/trivia/static/media/bricks-texture.8d3ce985.png"
+  },
+  {
+    "revision": "459d04f36694e6e0ceea79dad652ea9a",
+    "url": "/trivia/static/media/bricks-texture3.459d04f3.png"
+  },
+  {
+    "revision": "4f95b1fe4a9bd0e1872ec00b88bf940b",
+    "url": "/trivia/static/media/castelo.4f95b1fe.gif"
+  },
+  {
+    "revision": "a4689103b59f82a72240123cebdb38f1",
+    "url": "/trivia/static/media/nino.a4689103.webp"
+  },
+  {
+    "revision": "2f0dff02ae3acbcc5a4855ede4077d1b",
+    "url": "/trivia/static/media/oie_transparent.2f0dff02.png"
+  },
+  {
+    "revision": "79b16114331900b67dc34f958a70fc4c",
+    "url": "/trivia/static/media/porteiro.79b16114.png"
+  },
+  {
+    "revision": "f418d2eb5a6d3f2936230c08da4cd8fe",
+    "url": "/trivia/static/media/rato.f418d2eb.gif"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia/static/media/trivia.466d153e.png"
+  }
+]);
